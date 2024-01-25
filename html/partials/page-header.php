42c4d0882fa895903dcbdd805d022cee0a32e0f9
kenechi <nsudekenechi2@gmail.com>
2024-01-25 14:30:13 +0100

    project setup

---
+++ html/partials/page-header.php
@@ -0,0 +1,13 @@
+<!-- PAGE-HEADER -->
+<div class="page-header">
+  <h1 class="page-title my-auto">@SPK@title</h1>
+  <div>
+    <ol class="breadcrumb mb-0">
+      <li class="breadcrumb-item">
+        <a href="javascript:void(0)">@SPK@subtitle</a>
+      </li>
+      <li class="breadcrumb-item active" aria-current="page">@SPK@title</li>
+    </ol>
+  </div>
+</div>
+<!-- PAGE-HEADER END -->
